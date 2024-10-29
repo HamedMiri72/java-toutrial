3476454a55ea34bfe8d6d1b711a4503798cab790
HamedMiri72 <hamedmiri72@gmail.com>
2024-10-29 20:43:02 +0000

    dictionary

--- HangmanApplication/Hangman.java
+++ HangmanApplication/Hangman.java
@@ -2,6 +2,7 @@ package HangmanApplication;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Hangman {
@@ -16,5 +17,28 @@ public class Hangman {
     ArrayList<String> dictionary = new ArrayList<>();
     private static FileReader fileReader;
     private static BufferedReader bufferedReader;
-    
+
+    public Hangman() throws IOException {
+        initializeStreams();
+        mysterWord = pickWord();
+        currentGuss = initializeCurrentGuess();
+    }
+
+    private StringBuilder initializeCurrentGuess() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'initializeCurrentGuess'");
+    }
+
+    private String pickWord() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pickWord'");
+    }
+
+    public void initializeStreams() throws IOException {
+       try {
+           File inFile = new File("dictionary.txt");
+       } catch (File e) {
+       }
+    }
+
 }
